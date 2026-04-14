b5aaa25df2c3ad849a9622acc222e3a96cfe3a76
Ryan <ryan.kurtz08@gmail.com>
2026-04-14 15:42:39 -0600

    Update Home.jsx

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1>Welcome to My Portfolio</h1>
         <p>This is the home page.</p>
         <div className="page-container">
-          <h1>Contact Me</h1>
+          <h2>Contact Me</h2>
           <p>Ryan Kurtz</p>
           <p>Longmont, Colorado</p>
           <p>Ryan.Kurtz08@gmail.com</p>
@@ -20,7 +20,7 @@ export default function Home() {
 
       {/* About me */}
       <div className="page-container">
-        <h1>About Me</h1>
+        <h2>About Me</h2>
         <p><strong>The Foundation:</strong><br /> A background rooted in the technical precision of high-end bike mechanics and the communication-heavy world of 7th-grade science. These roles taught me how to deconstruct complex systems and translate technical "how" into human "why." <br /><br />
           <strong>The Stack:</strong><br /> Currently building responsive, performance-driven applications using React, JavaScript, and Tailwind CSS. Back-end focused in MySQL and Node.js, with a specialization in Generative AI integration through Purdue University. <br /><br />
           <strong>The Projects:</strong><br /> From architecting bike geometry visualization tools to developing full-scale HR portals, I focus on writing clean, maintainable code that solves niche problems. <br /><br />
@@ -30,39 +30,40 @@ export default function Home() {
       {/* Dev Projects */}
 
       <div className="page-container">
-        <h1>Dev</h1>
+        <h2>Dev Projects</h2>
         <h5>Velo22</h5>
         <p>Velo22 is a cycling community website it Jackson, WY. I started this website in order to create a central hub for routes, events, group rides, and more in Teton County. While I made this with squarespace, It taught me so much about designing and running a website, and was the catalyst to dive into software developement </p>
         <a href="https://velo22.org" target="_blank" rel="noopener noreferrer">Visit Velo22</a>
         <br /><br />
 
         <h5>GeoMatch</h5>
-        <p>GeoMatch is a bicycle geometry visualization tool designed to bridge the gap between different frame designs. By algorithmically comparing reach, stack, and angles, the app allows builders and sellers to match fit coordinates across disparate frames, ensuring a consistent ride feel regardless of the chassis. This project is currently in progress, and will hopefully be used in bike shops in the future. </p> <br /><br />
+        <p>GeoMatch is a bicycle geometry visualization tool designed to bridge the gap between different frame designs. By algorithmically comparing reach, stack, and angles, the app allows builders and sellers to match fit coordinates across disparate frames, ensuring a consistent ride feel regardless of the chassis. This project is currently in progress, and will hopefully be used in bike shops in the future. </p> <br />
+        <p>built with React, MongoDB, Express, Node.js</p><br />
 
         <h5>Small Projects</h5>
-        <p>In my github, you will see several projects from my Purdue Software Developement Program that were practice using: <ul><li>React</li><li>JavaScript</li><li>Tailwind CSS</li><li>MySQL</li><li>MongoDB</li><li>Express</li><li>Node.js</li><li>Generative AI</li><li>Framework Testing</li></ul> </p>
+        <p>In my github, you will see several projects from my Purdue Software Developement Program that were practice using:</p> <ul><li>React</li><li>JavaScript</li><li>Tailwind CSS</li><li>MySQL</li><li>MongoDB</li><li>Express</li><li>Node.js</li><li>Generative AI</li><li>Framework Testing</li></ul> 
       </div>
 
 
       {/* Education */}
-      <section class="education-section">
-        <div class="timeline">
-          <div class="container left">
-            <div class="content">
-              <h1>Education</h1>
+      <section className="education-section">
+        <div className="timeline">
+          <div className="container left">
+            <div className="content">
+              <h2>Education</h2>
               <h3>March 2026</h3>
               <h2>Purdue University</h2>
-              <p class="degree">Full Stack Development with Generative AI</p>
-              <p class="details">Intensive program focused on MERN stack (MongoDB, Express, React, Node) and AI-driven development workflows.</p>
+              <p className="degree">Full Stack Development with Generative AI</p>
+              <p className="details">Intensive program focused on MERN stack (MongoDB, Express, React, Node) and AI-driven development workflows.</p>
             </div>
           </div><br /><br />
 
-          <div class="container right">
-            <div class="content">
+          <div className="container right">
+            <div className="content">
               <h3>2017</h3>
               <h2>Colorado State University</h2>
-              <p class="degree">B.S. Natural Sciences, Biology Education</p>
-              <p class="details"></p>
+              <p className="degree">B.S. Natural Sciences, Biology Education</p>
+              <p className="details"></p>
             </div>
           </div>
         </div>
