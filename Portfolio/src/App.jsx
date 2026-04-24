65e0a076207411beeeea9f45a1b838407bd38b98
Ryan <ryan.kurtz08@gmail.com>
2026-04-23 18:19:43 -0600

    publish

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/ryank-portfolio/">
+    <Router>
       <Navbar />
       <div className="main-content">
         <Routes>
